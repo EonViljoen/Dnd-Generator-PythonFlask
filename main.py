8cfcffd75316e2f776c362f75c520aa6efbc9966
Eon Viljoen <Eon.Viljoen@dariel.co.za>
2023-10-11 14:39:07 +0200

    Got flask web server working with current test methods

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 import json
 
@@ -31,14 +31,24 @@ class JsonReader():
 #     def get(self):
 #         return data, 200
 
-# app = Flask(__name__)
+app = Flask(__name__)
 # api = Api(app)
-
 JsonReader()
 
+@app.route('/', methods=['GET'])
+def Home():
+    return jsonify(JsonReader.ReadFrom())
+
+@app.route('/<string:category>', methods=['GET'])
+def Category(category):
+    arg = request.args.get('arg', default=None, type=str)
+    if arg is not None:
+        return jsonify(JsonReader.ReadFrom(category, arg))
+    else:
+        return  jsonify(JsonReader.ReadFrom(category))
 
 # api.add_resource(Generator, '/')
 
 if __name__ == '__main__':
-    # app.run()
-    print(json.dumps(JsonReader.ReadFrom("Encounter", "Aerial Encounters")))
+    app.run(debug=True)
+    # print(json.dumps(JsonReader.ReadFrom("Encounter", "Aerial Encounters")))
