e9b6d7c4d9e63022f6c15e5306fcb82ed9ec47f2
Eon Viljoen <Eon.Viljoen@dariel.co.za>
2023-12-19 13:43:52 +0200

    Able to retrieve response from json and display in toast

--- static/scripts/toasts.js
+++ static/scripts/toasts.js
@@ -1,32 +1,22 @@
-const toastTrigger = document.getElementById('puzzleTest')
-const toastLiveExample = document.getElementById('liveToast')
-var toastResponse = null
+const toastTrigger = document.getElementById('puzzleTest');
+const toastLiveExample = document.getElementById('liveToast');
+var toastResponse = null;
 
 if (toastTrigger) {
   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
   toastTrigger.addEventListener('click', () => {
-    console.log(toastResponse)
-    toastBootstrap.show();
+    
     fetch('http://127.0.0.1:5000/r/Dungeon?arg=Puzzles')
             .then(res => res.json())
             .then(data => {
-                toastResponse = data
-                console.log(data)
+              document.getElementById('toast-response').innerText = data;
             });
+
+            toastBootstrap.show();
   })
 }
-
-// var entryResponse;
-        // const toastResponse = document.getElementById('toast-response')
-        // console.log(toastResponse)
-
-        // fetch('http://127.0.0.1:5000/r/Dungeon?arg=Puzzles')
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         entryResponse = data
-        //         console.log(data)
-        //     })
-        //     .then(() =>{
-        //         console.log(entryResponse)
-        //         toastResponse.innerText = 'entryResponse'
-        //     })
+function test(){
+  if (toastResponse !== null) {
+    
+  }
+}
